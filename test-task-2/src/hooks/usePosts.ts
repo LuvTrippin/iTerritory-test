a95553892bd64450d6daf2f3dfc491d53c6844a7
LuvTrippin <sergeevtimursh@gmail.com>
2026-07-07 20:22:54 +0500

    feat: Настройка TanStack Query, добавил хуки для работы с posts

---
+++ test-task-2/src/hooks/usePosts.ts
@@ -0,0 +1,52 @@
+import {queryOptions, useMutation, useQuery, useQueryClient} from '@tanstack/react-query';
+import type {Post, PostFormData} from "../types";
+import {postApi} from "../api/postsApi.ts";
+
+
+export const postsQueryOptions = queryOptions({
+    queryKey: ["posts"],
+    queryFn: postApi.getAll,
+});
+
+export function usePosts() {
+    return useQuery(postsQueryOptions);
+}
+
+export function useCreatePost() {
+    const queryClient = useQueryClient();
+
+    return useMutation({
+        mutationFn: (data: PostFormData) => postApi.create(data),
+        onSuccess: (newPost) => {
+            queryClient.setQueryData<Post[]>(postsQueryOptions.queryKey, (old) =>
+                old ? [newPost, ...old] : [newPost],
+            )
+        },
+    })
+}
+
+export function useUpdatePost() {
+    const queryClient = useQueryClient();
+
+    return useMutation({
+        mutationFn: ({id, data}: {id: number, data: PostFormData}) => postApi.update(id, data),
+        onSuccess: (updatedPost) => {
+            queryClient.setQueryData<Post[]>(postsQueryOptions.queryKey, (old) =>
+                old?.map(post => (post.id === updatedPost.id ? updatedPost : post)),
+            );
+        }
+    })
+}
+
+export function useDeletePost() {
+    const queryClient = useQueryClient();
+
+    return useMutation({
+        mutationFn: (id: number) => postApi.delete(id),
+        onSuccess: (_, id) => {
+            queryClient.setQueryData<Post[]>(postsQueryOptions.queryKey, (old) =>
+                old?.filter((post) => post.id !== id),
+            );
+        }
+    })
+}
